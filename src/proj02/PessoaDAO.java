a50e61e79634dff3d0e8b42b21a24143cf331232
aluno <aluno@sj-labprog-866461>
2018-08-24 09:20:50 -0300

    Adicionado listar todos

--- src/proj02/PessoaDAO.java
+++ src/proj02/PessoaDAO.java
@@ -2,7 +2,10 @@ package proj02;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -27,5 +30,36 @@ public class PessoaDAO {
         }
 
         return true;
+    }
+    
+    public List<Pessoa> listarTodas(){
+        List<Pessoa> pessoas = new ArrayList<>();
+        
+        try(Connection conexao = ConnectionFactory.getConnection()){
+            String sql = "SELECT * from Pessoa";
+            
+            try(PreparedStatement stmt = conexao.prepareStatement(sql);
+                    ResultSet rs = stmt.executeQuery()){
+                
+                while(rs.next()){
+                    Pessoa c = new Pessoa(
+                            rs.getString("nome"),
+                            rs.getString("telefone"),
+                            rs.getString("email"),
+                            rs.getDouble("peso"),
+                            rs.getInt("altura")
+                    );
+                    
+                    c.setIdPessoa(rs.getInt("idPessoa"));
+                    pessoas.add(c);
+                }
+                
+            }
+            
+        } catch(SQLException ex){
+            System.err.println(ex.toString());
+        }
+        
+        return pessoas;
     }
 }
--- src/proj02/Principal.java
+++ src/proj02/Principal.java
@@ -1,12 +1,19 @@
 package proj02;
 
+import java.util.Iterator;
+import java.util.List;
+
 public class Principal {
 
     public static void main(String[] args) {
         PessoaDAO pDao = new PessoaDAO();
-        Pessoa p = new Pessoa("Gabriel", "1234", "gabriel@turnez.com.br", 80, 180);
-        
-        pDao.inserir(p);
+        //Pessoa p = new Pessoa("Gabriel", "1234", "gabriel@turnez.com.br", 80, 180);
+        //pDao.inserir(p);
+    
+        List<Pessoa> contatos = pDao.listarTodas();
+        for (Pessoa contato : contatos) {
+            System.out.println(contato.getNome());
+        }
     }
 
 }
